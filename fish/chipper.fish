7d0153aaf9b8364791dca67a9d1ce7152d904813
Ranjan Srinivas <rsrinivas@chippercash.com>
2023-07-17 10:47:22 -0700

    commit

--- fish/chipper.fish
+++ fish/chipper.fish
@@ -5,5 +5,5 @@ abbr eval_card_dataset "bazel run pipelines/offline/card_deactivation:eval_data"
 abbr eval_card_run "bazel run pipelines/offline/card_deactivation:run_evals"
 
 # heroku commands
-abbr hcomp "heroku run -a chipper-compliance-production "NODE_PATH=build node build/jobs""
-abbr hcoms "heroku run -a chipper-compliance-staging "NODE_PATH=build node build/jobs""
+abbr hcomp 'heroku run -a chipper-compliance-production "NODE_PATH=build node build/jobs"'
+abbr hcoms 'heroku run -a chipper-compliance-staging "NODE_PATH=build node build/jobs"'
