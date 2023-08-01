0fd0d2285a18d1630b45c06a50b3f7f7151d1983
Ranjan Srinivas <rsrinivas@chippercash.com>
2023-08-01 09:36:55 -0700

    commit

--- fish/chipper.fish
+++ fish/chipper.fish
@@ -7,3 +7,9 @@ abbr eval_card_run "bazel run pipelines/offline/card_deactivation:run_evals"
 # heroku commands
 abbr hcomp 'heroku run -a chipper-compliance-production "NODE_PATH=build node build/jobs"'
 abbr hcoms 'heroku run -a compliance-staging-p "NODE_PATH=build node build/jobs"'
+
+# gcloud
+abbr gcloud_chipper 'gcloud beta compute ssh --zone "us-east4-c" "chipper-prod-intelligence"  --project "chipper-analysis"'
+
+# testing
+abbr ntest 'npm run test -- -- "app/"'
